5ff960bf853c0a86be55ea67844f9418decd8851
Yash-bhagwat <yashbhagwatyaan@gmail.com>
2024-03-12 14:22:58 +0530

    teamrater for group of 3

---
+++ teamRateCalcmax3group.py
@@ -0,0 +1,59 @@
+# Prints teamrating for a bunch of users grouped into groups of 3 using their max ratings
+# Usage : python3 teamRateCalc.py {list of handles}
+# for eg. : python3 teamRateCalcmax.py {Blitztage Nisanth Teja-Smart ... }
+
+import math
+import sys
+
+import requests
+
+n = len(sys.argv)
+if n == 1:
+    print("Invalid query")
+    sys.exit()
+team = [sys.argv[i] for i in range(1, n)]
+
+
+def getwinprob(ra, rb):
+    return 1 / (1 + math.pow(10, (rb - ra) / 400))
+
+
+def teamrate(given_ratings):
+    left, right = 1, 10000
+    for _ in range(100):
+        mid = (left + right) / 2
+        winprob = 1.0
+        for v in given_ratings:
+            winprob *= getwinprob(mid, v)
+        rating = math.log10(1 / winprob - 1) * 400 + mid
+        if rating > mid:
+            left = mid
+        else:
+            right = mid
+    return round((left + right) / 2)
+
+def batchto3(given_ratings):
+    n = len(given_ratings) // 3
+    for i in range(n):
+        print(teamrate([given_ratings[i * 3], given_ratings[i * 3 + 1], given_ratings[i * 3 + 2]]))
+
+ratings = []
+template = "https://codeforces.com/api/user.rating?handle="
+for person in team:
+    response = requests.get(template + person)
+    response = response.json()
+    if response["status"] == "FAILED":
+        print(f"{person} is an invalid handle")
+        sys.exit()
+    response = response["result"]
+    if len(response) == 0:
+        ratings.append(0)
+        print(person, 0)
+    else:
+        best = 0
+        for indiv in response:
+             best = max(best, indiv["newRating"])
+        ratings.append(best)
+        print(person, best)
+batchto3(ratings)
+# print(teamrate(ratings))
